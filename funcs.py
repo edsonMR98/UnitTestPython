f7eadf00a367150a6ff6bc4e8ead1e074f21ada6
Edson Moreno <edsonmorenoruiz@gmail.com>
2020-01-30 12:14:45 -0600

    Docs function was added in order to doctest can test them

--- funcs.py
+++ funcs.py
@@ -1,11 +1,39 @@
 def suma(a, b):
+    """
+    Returns a + b
+
+    >>> suma(5, 7)
+    12
+    """
     return a + b
 
 def resta(a, b):
+    """
+    Returns a - b
+
+    >>> resta(10, 7)
+    3
+    """
     return a - b
 
 def multiplicacion(a, b):
+    """
+    Returns a * b
+
+    >>> multiplicacion(5, 7)
+    35
+    """
     return a * b
 
 def division(a, b):
+    """
+    Returns a / b
+
+    >>> division(10, 5)
+    2
+    """
     return a / b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
